ecf19cc9607c6f2880f7b39e59cb6a788616ec0b
Elvin Belvin <martynas.raiselis@gmail.com>
2018-01-11 14:05:26 +0100

    something

---
+++ countdown.js
@@ -0,0 +1,26 @@
+// Counting down from 8 days in the future minus 1 milisecond
+var countDownDate = 691199999 + new Date().getTime();
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = days + "  " + hours + "  "
+    + minutes + "  " + seconds + "";
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
